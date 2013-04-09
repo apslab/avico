1f7028ad082fb3a839e1679d34950de35d6ddd64
luis petek <lmpetek@gmail.com>
2013-04-09 15:40:29 -0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avico::Application.config.secret_token = 'f534a8599b9b099333450f082b79a599a26277a255bb69e2be176e2f599eaf42b95117f3cc6cca922cb7bf87f82215f258c477bc508d5596653090b144907b93'
